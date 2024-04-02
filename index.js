267cb71dda92f5b154a31e1c2e476026101d7ec7
rasmus-kaikkonen <t3kara00@students.oamk.fi>
2024-04-02 17:40:56 +0300

    Implemented login and registration functionality

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const { userRouter } = require('./routes/user');
 
 const port = 3001;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+app.use('/user',userRouter);
+
+app.listen(port,() => {
+    console.log(`Server is listening on port ${port}`);
+});
