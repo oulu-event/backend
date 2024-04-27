3f852305b2cbb31d69e7424ac38389cbd5ace750
rasmus-kaikkonen <t3kara00@students.oamk.fi>
2024-04-28 01:51:42 +0300

    Fixed some typos in database.sql file

--- database.sql
+++ database.sql
@@ -1,6 +1,18 @@
 drop table if exists events;
 drop table if exists users;
 drop table if exists join_request;
+drop table if exists comments;
+drop table if exists notification;
+drop table if exists participants;
+
+create table users (
+	id serial primary key,
+	firstname varchar(100),
+	lastname varchar(100),
+	email varchar(100),
+	password varchar(100),
+	dob date
+);
 
 create table events (
 	id serial primary key,
@@ -15,18 +27,9 @@ create table events (
 				references users(id)
 );
 
-create table users (
-	id serial primary key,
-	firstname varchar(100),
-	lastname varchar(100),
-	email varchar(100),
-	password varchar(100),
-	dob date
-);
-
 create table join_request (
 	id serial primary key,
-	status int
+	status int,
 	event_id int not null,
 		constraint fk_event
 			foreign key(event_id)
@@ -53,7 +56,7 @@ create table comments (
 create table notification (
 	id serial primary key,
 	title varchar(100),
-	desc varchar(255),
+	description varchar(255),
 	user_id int not null,
 		constraint fk_user
 			foreign key(user_id)
